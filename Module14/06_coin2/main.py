475ab7ef0d8728fe38b31ccd30c1dcd26e900785
lebedevkn@mail.ru <VWP@$$@TB5>
2022-10-22 00:43:28 +0300

    done "coin2"

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+
+
+def scan(x, y, r):
+    return (x * x + y * y) ** 0.5 < r
+
+
+print('Введите координаты монетки:')
+point_x = float(input('X: '))
+point_y = float(input('Y: '))
+radius = int(input('Введите радиус: '))
+
+if scan(point_x, point_y, radius):
+    print('Монетка где-то рядом.')
+else:
+    print('Монетки в области нет.')
