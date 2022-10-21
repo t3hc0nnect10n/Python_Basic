260bab8b5c698e3b3e21ccd8a59bcedc00597e52
lebedevkn@mail.ru <VWP@$$@TB5>
2022-10-22 00:30:18 +0300

    done "reverse_num"

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,19 @@
 # TODO здесь писать код
 
+def number_reverse(n):
+    parts = str(n).split('.')
+    parts[0] = ''.join(reversed(list(str(parts[0]))))
+    parts[1] = ''.join(reversed(list(str(parts[1]))))
+    return float(parts[0] + '.' + parts[1])
 
+
+first_number = float(input('Введите первое число: '))
+second_number = float(input('Введите второе число: '))
+
+first_number_reverse = number_reverse(first_number)
+second_number_reverse = number_reverse(second_number)
+summ = first_number_reverse + second_number_reverse
+
+print(f'Первое число наоборот: {first_number_reverse}')
+print(f'Второе число наоборот: {second_number_reverse}')
+print(f'Сумма: {summ}')
