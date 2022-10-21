8f69378e59395c022b897fdad80e523e30166e66
lebedevkn@mail.ru <VWP@$$@TB5>
2022-10-22 00:32:10 +0300

    done "least_divisor"

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+def lcd(a):
+    lcd_num = 1
+
+    for num in range(1, a + 1):
+        if a % num == 0:
+            lcd_num = num
+        if lcd_num > 1:
+            break
+    return lcd_num
+
+
+number = lcd(int(input('Введите число: ')))
+print(f'Наименьший делитель, отличный от единицы: {number}')
