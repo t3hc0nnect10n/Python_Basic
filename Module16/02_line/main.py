a3b98671071a4368d03975b490ea897f4265585b
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-11 22:22:41 +0300

    task_02 is done

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+first_class = []
+second_class = []
+new_class = []
+
+for i in range(160, 178, 2):
+    first_class.append(i)
+print(f'Первый класс: {first_class}')
+
+for i in range(162, 183, 3):
+    second_class.append(i)
+print(f'Второй класс: {second_class}')
+
+new_class.extend(sorted(first_class + second_class))
+print(f'Отсортированный список учеников: {new_class}')
+
