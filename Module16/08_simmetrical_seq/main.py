e85b98832a5ab2e498ad4f9cbefb0a6a988b4bcc
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-13 07:36:03 +0300

    task_08 is done

--- Module16/08_simmetrical_seq/main.py
+++ Module16/08_simmetrical_seq/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+numbers = int(input('Количество чисел: '))
+number_list = []
+reverse_list = []
+
+for _ in range(1, numbers + 1):
+    num = int(input('Число: '))
+    number_list.append(num)
+
+print(f'Последовательность: {number_list}')
+num_i = int(input('Нужно приписать чисел: '))
+
+for i in range(num_i):
+    reverse_list.append(number_list[i])
+
+print(f'Сами числа: {reverse_list[::-1]}')
