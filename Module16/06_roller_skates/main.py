49ec569de5dc3ce4f69cc5d46e354aa0a24779f5
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-13 05:47:35 +0300

    task_06 is done

--- Module16/06_roller_skates/main.py
+++ Module16/06_roller_skates/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+skates_list = []
+human_list = []
+count = 0
+
+skates = int(input('Количество коньков: '))
+for i in range(skates):
+    size_skate = int(input(f'Размер пары {i + 1}: '))
+    skates_list.append(size_skate)
+
+human = int(input('Количество людей: '))
+for num_hum in range(human):
+    size_man = int(input(f'Размер ноги человека {num_hum + 1}: '))
+    human_list.append(size_man)
+
+for num_skate in human_list:
+    if num_skate in skates_list:
+        skates_list.remove(num_skate)
+        count += 1
+
+print(f'Наибольшее количество людей, которые могут взять ролики: {count}')
