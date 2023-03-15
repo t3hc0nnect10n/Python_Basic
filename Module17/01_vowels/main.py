3f9319c50b5241578fee2a79972dfb36c08b53a7
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-15 07:04:20 +0300

    task_01 is done

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+vowels = 'ауоыиэяюёе'
+text = input('Введите текст: ')
+list_vowels = [i for i in text if i in vowels]
+
+print(f'Список гласных букв: {list_vowels}'
+      f'\nДлина списка: {len(list_vowels)}')
