41c3bdf7d502e89726c3200da1a4c308f114d26a
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-16 05:46:50 +0300

    task_04 is done

--- Module17/04_slices/main.py
+++ Module17/04_slices/main.py
@@ -1,3 +1,12 @@
 alphabet = 'abcdefg'
 
-# TODO здесь писать код
+print(  f'01: {alphabet[:]}'
+      f'\n02: {alphabet[::-1]}'
+      f'\n03: {alphabet[::2]}'
+      f'\n04: {alphabet[1::2]}'
+      f'\n05: {alphabet[:1]}' 
+      f'\n06: {alphabet[-1]}' 
+      f'\n07: {alphabet[3:4]}' 
+      f'\n08: {alphabet[-3:]}'
+      f'\n09: {alphabet[3:5]}'
+      f'\n10: {alphabet[3:5][::-1]}')
