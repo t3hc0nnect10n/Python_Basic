f23210c8f325c8865ef295c5565efb24d4853503
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-16 07:21:30 +0300

    task_05 is done

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+
+letter_index = [i for i, letter in enumerate(text) if letter == 'h']
+start = letter_index[0]
+end = letter_index[len(letter_index) - 1] - 1
+
+print(f'Развернутая последовательность между первым и последним h: {text[end:start:-1]}')
