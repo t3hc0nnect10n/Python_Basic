b1d708176ccb86bceba305c0a1cc7cdf3b738835
lebedevkn@mail.ru <VWP@$$@TB5>
2023-03-16 05:25:56 +0300

    task_03 is done

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+team_a = [round(random.uniform(5, 10), 2) for a in range(20)]
+team_b = [round(random.uniform(5, 10), 2) for b in range(20)]
+team_win = [(team_a[i] if team_a[i] > team_b[i] else team_b[i]) for i in range(20)]
+
+print(f'Первая команда: {team_a}'
+      f'\nВторая команда: {team_b}'
+      f'\nПобедители тура: {team_win}')
