43f69538d50c6e685220ff9713e7fc41ef2f7da4
lebedevkn@mail.ru <VWP@$$@TB5>
2023-10-09 17:30:55 +0300

    task_03 is done

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+while True:
+    file_name = input('Название файла: ')
+
+    sym = file_name.startswith(('@', '№', '$', '%', '^', '&', '*', '(', ')'))
+    extension = file_name.endswith('.txt') or file_name.endswith('.docx')
+
+    if sym:
+        print('\nОшибка: название начинается на один из специальных символов')
+    elif sym or not extension:
+        print('\nОшибка: неверное расширение файла. Ожидалось .txt или .docx')
+    else:
+        print('\nФайл назван верно.')
+        break
