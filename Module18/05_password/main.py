dd62530a095e227c62de89dfdbd0c99c773e34f4
lebedevkn@mail.ru <VWP@$$@TB5>
2023-12-09 23:54:48 +0300

    task_05 is done

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+while True:
+    password = input('Придумайте пароль: ')
+    if (len(password) >= 8) and (password.isalnum()) and (password.islower()) == (password.isupper()):
+        print('Это надежный пароль!')
+        break
+    else:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
