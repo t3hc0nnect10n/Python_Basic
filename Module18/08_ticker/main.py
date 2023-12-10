6bc5269ffb8ed58c65a2dc0350180fbedfb69609
lebedevkn@mail.ru <VWP@$$@TB5>
2023-12-11 01:44:07 +0300

    task_08 is done

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+first_string = input('Первая строка: ')
+second_string = input('Вторая строка: ')
+
+for shift in range(len(second_string)):
+    if first_string == second_string:
+        print(f'Первая строка получается из второй со сдвигом {shift}.')
+        break
+    second_string = second_string[1:] + second_string[0]
+else:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
