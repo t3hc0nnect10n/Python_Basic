5ad4992c166e0264f3f437607aebe1cef1b9672f
lebedevkn@mail.ru <VWP@$$@TB5>
2023-12-11 02:09:44 +0300

    task_09 is done

--- Module18/09_comment_analysis/main.py
+++ Module18/09_comment_analysis/main.py
@@ -1,5 +1,15 @@
-# TODO здесь писать код
+def count_uppercase_lowercase(upp_low):
+    count_upp = 0
+    count_low = 0
 
+    for i in upp_low:
+        if i.isupper():
+            count_upp += 1
+
+        elif i.islower():
+            count_low += 1
+
+    return count_upp, count_low
 
 # Пример использования:
 text = input("Введите строку для анализа: ")
