ba021cc7146117f04872a8279a818445c63a9dc6
lebedevkn@mail.ru <VWP@$$@TB5>
2023-12-20 03:06:13 +0300

    task_05 corrected

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,14 +1,18 @@
 # Функция "latin" проверяет пароль на наличие латинских букв
 def latin(text):
-    count = 0
+    count_1 = 0
+    count_2 = 0
     for i in text:
         if i.encode().isalpha():
-            count += 1
+            count_1 += 1
+        elif i.encode('utf-8') and not i.isdigit():
+            count_2 += 1
 
-    if count >= 1:
+    if count_1 >= 1 and count_2 == 0:
         return True
     else:
         return False
+
 
 # Функция "low" проверяет пароль на наличие прописной буквы
 def low(text):
@@ -22,6 +26,7 @@ def low(text):
     else:
         return False
 
+
 # Функция "upp" проверяет пароль на наличие заглавной буквы
 def upp(text):
     count = 0
@@ -34,6 +39,7 @@ def upp(text):
     else:
         return False
 
+
 # Функция "digit_min_three" проверяет пароль на наличие минимум трёх цифр
 def digit_min_three(text):
     count = 0
@@ -45,6 +51,7 @@ def digit_min_three(text):
         return True
     else:
         return False
+
 
 # В цикле проверяется выполнение условий к требованиям сложности пароля
 while True:
