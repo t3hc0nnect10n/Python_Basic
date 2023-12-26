347da3a353a65dca65aeca09cb5de338fca34dcb
lebedevkn@mail.ru <VWP@$$@TB5>
2023-12-26 12:25:42 +0300

    task_01 is done

--- Module19/01_songs_2/main.py
+++ Module19/01_songs_2/main.py
@@ -10,4 +10,12 @@ violator_songs = {
     'Clean': 5.83
 }
 
-# TODO здесь писать код
+num_track = int(input('Сколько песен выбрать? '))
+
+sum_time = 0
+for i in range(num_track):
+    name_track = input(f'Название {i+1} песни: ')
+    if name_track in violator_songs:
+        sum_time += violator_songs.get(name_track)
+
+print(f'Общее время звучания песен: {round(sum_time, 2)} минуты')
