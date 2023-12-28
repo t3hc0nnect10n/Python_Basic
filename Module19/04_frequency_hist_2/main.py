5ccf9466edc1086b36f7f8da825ef6e07a41136f
lebedevkn@mail.ru <VWP@$$@TB5>
2023-12-28 06:10:31 +0300

    task_04 is done

--- Module19/04_frequency_hist_2/main.py
+++ Module19/04_frequency_hist_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+string = 'здесь что-то написано' # input('Введите текст: ')
+
+hist = {sym: string.count(sym) for sym in string}
+
+print('\nОригинальный словарь частот:')
+for key in sorted(hist.keys()):
+    print(key, ':', hist[key])
+
+invert_hist = {val: [i_key for i_key in hist.keys() if hist[i_key] == val] for val in set(hist.values())}
+
+print('\nИнвертированный словарь частот:')
+for invert_key in invert_hist:
+    print(f'{invert_key} : {invert_hist[invert_key]}')
