4cc40754b38755a51c7d35284876a034adb7d512
lebedevkn@mail.ru <VWP@$$@TB5>
2024-02-03 15:59:38 +0300

    task_04 has been corrected

--- Module19/04_frequency_hist_2/main.py
+++ Module19/04_frequency_hist_2/main.py
@@ -6,8 +6,15 @@ print('\nОригинальный словарь частот:')
 for key in sorted(hist.keys()):
     print(key, ':', hist[key])
 
-invert_hist = {val: [i_key for i_key in hist.keys() if hist[i_key] == val] for val in set(hist.values())}
+# invert_hist = {val: [i_key for i_key in hist.keys() if hist[i_key] == val] for val in set(hist.values())}
 
 print('\nИнвертированный словарь частот:')
-for invert_key in invert_hist:
-    print(f'{invert_key} : {invert_hist[invert_key]}')
+invert_hist = {}
+for key in hist.keys():
+    val = hist[key]
+    if val not in invert_hist:
+        invert_hist[val] = []
+    invert_hist[val].append(key)
+
+for key in sorted(invert_hist.keys()):
+    print(key, ':', invert_hist[key])
