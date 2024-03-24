c28419c4e2bc67b4cad4a906a3283d99cff2c3e9
lebedevkn@mail.ru <VWP@$$@TB5>
2024-03-24 18:47:14 +0300

    task_04 is done

--- Module20/04_pairs/main.py
+++ Module20/04_pairs/main.py
@@ -1,1 +1,4 @@
-# TODO здесь писать код
+number_list = [i for i in range(0, 10)]
+new_number_list = list(zip(number_list[::2], number_list[1::2]))
+print(f'Оригинальный список: {number_list}\n'
+      f'Новый список: {new_number_list}')
